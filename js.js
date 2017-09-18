10fea6368cf1cbe25a1b6b5b8b090f7b3c4c2a93
DanielLeczkowski <daniel.leczkowski@wp.pl>
2017-09-18 02:06:00 +0200

    refresh background and logo

--- js.js
+++ js.js
@@ -46,6 +46,12 @@ $(function () {
     })
 });
 
+$(document).on('click', 'a', function(event){
+	    event.preventDefault();
+	    $('body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 1000);
+	});
 
 
 
